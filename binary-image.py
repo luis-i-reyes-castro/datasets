252951b086da1c4fea8cf8b6f9fbcbd50d42370e
amjalca <amjalca@users.noreply.github.com>
2017-05-24 10:11:36 -0500

    Update binary-image.py

--- binary-image.py
+++ binary-image.py
@@ -9,6 +9,7 @@ import numpy as np
 from scipy.misc import imread, imresize
 from scipy import ndimage
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 ## list_name_image is path and id_row
 ## 010(Name file) (0--> id row) (10--> id column)
@@ -17,20 +18,28 @@ def readfile(list_name_image, id_row):
     for file in list_name_image:
         if file.startswith(id_row):
             image = imread(file)
-            images.append(image)
-            ##type(image) <class 'numpy.ndarray'>
+            images.append(image) #<class 'numpy.ndarray'> #shape 480 640 3
     return images
             
 ## Use ITU-R 601-2 luma transform 
 ## L = R * 299/1000 + G * 587/1000 + B * 114/1000
 def rgb2gray(rgb):
-    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+    image_gray = np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+    return image_gray
 
+#image_file = readfile(os.listdir(os.getcwd()), "0")
 
-image_file = readfile(os.listdir(os.getcwd()), "0")
-print(len(image_file))
-#image = imread('1id/00.jpg')
+#print(image_file)
+image = imread('/home/luiireye/Desktop/original-dataset/binario/1id/00.jpg')
 ##plt.imshow(image)
-#image_gray = rgb2gray(image)
+#img_gray = rgb2gray(image)
 #plt.imshow(image_gray, cmap=plt.cm.gray) 
 
+#Most significant bit
+image_blue = image[:,:,0]
+
+image_blue_b = image_blue.ravel()
+
+image_blue_b = image_blue >= 128
+
+image_blue_b_lsb = image_blue & 1
