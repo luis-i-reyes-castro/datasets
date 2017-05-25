b85941a188619edd745e041eb428d0e1755f4074
amjalca <amjalca@users.noreply.github.com>
2017-05-25 10:24:05 -0500

    Update binary-image.py

--- binary-image.py
+++ binary-image.py
@@ -6,6 +6,8 @@ Created on Thu May 18 08:43:38 2017
 """
 import os, sys
 import numpy as np 
+from sklearn.utils import check_array
+from sklearn import preprocessing
 from scipy.misc import imread, imresize
 from scipy import ndimage
 import matplotlib.pyplot as plt
@@ -27,19 +29,25 @@ def rgb2gray(rgb):
     image_gray = np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
     return image_gray
 
+##Intensity division (Threshold)
+def rgb2b(rgb2d, level): 
+    binary = preprocessing.Binarizer(level).fit(rgb2d)
+    image_binary = binary.transform(rgb2d)
+    return image_binary
+
+##Dataset 3d to 2d array
+def image2d(rgb3d):
+    ndata, nx, ny = rgb3d.shape
+    image_2d = rgb3d.reshape(ndata,nx*ny)
+    return image_2d
+    
 #image_file = readfile(os.listdir(os.getcwd()), "0")
 
 #print(image_file)
 image = imread('/home/luiireye/Desktop/original-dataset/binario/1id/00.jpg')
+im_pr_2d = image2d(image)
+image_b = rgb2b(im_pr_2d, 128) ##I need change 2d to 3d image result
+plt.imshow(image_b)
 ##plt.imshow(image)
 #img_gray = rgb2gray(image)
 #plt.imshow(image_gray, cmap=plt.cm.gray) 
-
-#Most significant bit
-image_blue = image[:,:,0]
-
-image_blue_b = image_blue.ravel()
-
-image_blue_b = image_blue >= 128
-
-image_blue_b_lsb = image_blue & 1
