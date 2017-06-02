03614450ac6b47a858a024a57f2fc2e1ff038c97
Luis I. Reyes-Castro <luis.i.reyes.castro@gmail.com>
2017-06-02 16:48:33 -0500

    Arregla archivo binary-image para que lea y muestre imagenes en blanco y negro.

--- binary-image.py
+++ binary-image.py
@@ -4,60 +4,17 @@
 Created on Thu May 18 08:43:38 2017
 
 """
-import os, sys
-import numpy as np 
-from sklearn.utils import check_array
-from sklearn import preprocessing
-from scipy.misc import imread, imresize
-from scipy import ndimage
+import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
+from scipy.misc import imread
 
-## list_name_image is path and id_row
-## 010(Name file) (0--> id row) (10--> id column)
-def readfile(list_name_image, id_row):
-    images = []
-    for file in list_name_image:
-        if file.startswith(id_row):
-            image = imread(file)
-            images.append(image) #<class 'numpy.ndarray'> #shape 480 640 3
-    return images
-            
-## Use ITU-R 601-2 luma transform 
-## L = R * 299/1000 + G * 587/1000 + B * 114/1000
-def rgb2gray(rgb):
-    image_gray = np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
-    return image_gray
+image0 = imread( '/home/luis/datasets/vision-01/1id/00.jpg', flatten = True)
+image1 = imread( '/home/luis/datasets/vision-01/1id/01.jpg', flatten = True)
 
-##Intensity division (Threshold)
-def rgb2b(rgb2d, level): 
-    binary = preprocessing.Binarizer(level).fit(rgb2d)
-    image_binary = binary.transform(rgb2d)
-    return image_binary
+todas = np.stack( [ image0, image1])
 
-##Dataset 3d to 2d array
-def image2d(rgb3d):
-    ndata, nx, ny = rgb3d.shape
-    image_2d = rgb3d.reshape(ndata,nx*ny)
-    return image_2d #<class 'numpy.ndarray'> 480, 1920
+imagen_prom = np.mean( todas, axis = 0)
+imagen_std  = np.std( todas)
 
-##Dataset 2d to 3d array
-def image3d(bw2d):
-    bwdata, bwx = bw2d.shape
-    image_3d = bw2d.reshape(bwdata, 640, 3)
-    #print (bwx)
-    return image_3d
-    
-#image_file = readfile(os.listdir(os.getcwd()), "0")
-
-#print(image_file)
-image = imread('/home/xxx/Desktop/original-dataset/binario/1id/00.jpg')
-#print (image.shape)
-im_pr_2d = image2d(image)
-image_b = rgb2b(im_pr_2d, 128) ##I need change 2d to 3d image result
-pr = image3d(image_b)
-print(pr.shape[2])
-#print (im_pr_2d.shape)
-plt.imshow(pr)
-#img_gray = rgb2gray(image)
-#plt.imshow(image_gray, cmap=plt.cm.gray) 
+todas_preprocesadas = ( todas - imagen_prom ) / imagen_std
+plt.imshow( todas_preprocesadas[0,:,:] )
