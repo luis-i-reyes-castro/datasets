6d89f673783e441ca314a2b0f04439b974ff7f82
Ana Jalca <amjalca@espol.edu.ec>
2017-06-28 12:18:28 -0500

    Completar binary image

--- src/binary-image.py
+++ src/binary-image.py
@@ -1,20 +1,22 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu May 18 08:43:38 2017
-
+Created on Thu Jun 22 10:07:00 2017
 """
+
+import os, sys
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.misc import imread
 
-image0 = imread( '/home/luis/datasets/vision-01/1id/00.jpg', flatten = True)
-image1 = imread( '/home/luis/datasets/vision-01/1id/01.jpg', flatten = True)
+images = []
 
-todas = np.stack( [ image0, image1])
-
-imagen_prom = np.mean( todas, axis = 0)
-imagen_std  = np.std( todas)
-
-todas_preprocesadas = ( todas - imagen_prom ) / imagen_std
-plt.imshow( todas_preprocesadas[0,:,:] )
+for root, dirnames, filenames in os.walk("/home/user/Documents/Investigacion/IA_Image/datasets/vision-02/"):    
+    for filename in filenames:
+        filepath = os.path.join(root, filename)
+        image = imread(filepath, flatten = True)
+        images.append(image)
+        todas = np.stack( [images])
+    imagen_prom = np.mean( todas, axis = 0)
+    imagen_std  = np.std( todas)
+    todas_preprocesadas = ( todas - imagen_prom ) / imagen_std
+    #plt.imshow( todas_preprocesadas[0,:,:] )
