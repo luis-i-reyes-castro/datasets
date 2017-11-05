18b686f71a54b95741e8500bd5f03b65e04bf540
Luis I. Reyes-Castro <luis.i.reyes.castro@gmail.com>
2017-11-05 05:20:18 -0500

    Gets something done. Hope its something good. Im not very sober right now.

--- scaled-models/sample-generator.py
+++ scaled-models/sample-generator.py
@@ -3,24 +3,40 @@ from datetime import datetime as dt
 from cv2 import VideoCapture, imwrite
 
 # Fraction of samples to be labeled and not-empty
-fraction_labeled  = 0.8
 fraction_nonempty = 0.8
-# Ships, headings and their respective fractions (probabilities)
-ships   = [ 'Cruiser-A', 'Cruiser-B', 'Cruiser-C', 'Freighter', 'Fishing' ]
-heading = [ 'East', 'West' ]
-fraction_ships   = [ 1./6, 1./6, 1./6, 1./6, 1./3 ]
-fraction_heading = [ 0.5, 0.5 ]
+
+# Rows, Columns, Headings and Ships
+rows     = [ '1', '2', '3', '4', '5', '6', '7' ]
+cols     = [ 'A', 'B', 'C', 'D' ]
+headings = [ 'East', 'West' ]
+ships    = [ 'Cruiser-1', 'Cruiser-2', 'Cruiser-3', 'Freighter',
+             'Fishing-1', 'Fishing-2', 'Empty' ]
+
+# Retrieves current date and time
+filename = str( dt.today() )[:19]
+filename = filename.replace( ' ', '_')
+# Randomly selects row, col, heading and ship
+r        = np.random.choice( rows)
+c        = np.random.choice( cols)
+h        = np.random.choice( headings)
+s        = np.random.choice( ships)
+filename = filename + '_Location:' + r + c \
+         + '_Heading:' + h + '_Ship:' + s
+
+# Prompts user to set up sample
+print( 'Please generate sample at: ' )
+print( 'LOCATION ' + r + c + ' HEADING ' + h + ' SHIP ' + s )
 
 # Initialize the camera capture object with the cv2.VideoCapture class.
 # All it needs is the index to a camera port. 
-camera = VideoCapture(0)
+camera = VideoCapture(1)
 key = raw_input( 'Are you ready? Press Enter to continue. ' )
 
 # Number of frames to throw away while the camera adjusts to light levels
 ramp_frames = 16
 # Ramp the camera - these frames will be discarded and are only used 
 # to allow v4l2 to adjust light levels, if necessary
-print( 'Ramping camera...' ) 
+print( 'Ramping camera...' )
 for i in xrange( ramp_frames) :
     return_val, image = camera.read()
 
@@ -29,9 +45,8 @@ print( 'Capturing actual image...' )
 return_val, image = camera.read()
 
 # Save the image in JPG format
-filename = str( dt.today() )[:19] + '.jpg'
-filename = filename.replace( ' ', '_')
-print( 'Writing image: ' + filename )
+filename += '.jpg'
+print( 'Writing image: ' + filename)
 imwrite( filename, image)
 
 # Release the camera so that other threads can use it later
