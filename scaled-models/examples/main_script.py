eac1bf470a37aa084521e6d4dbc2dab15273b9a9
Luis I. Reyes-Castro <luis.i.reyes.castro@gmail.com>
2017-12-27 21:14:54 -0500

    Starts experimenting with the Keras ImageDataGenerator.

--- scaled-models/examples/main_script.py
+++ scaled-models/examples/main_script.py
@@ -3,10 +3,22 @@
 @author: Luis I. Reyes-Castro
 """
 
+import numpy as np
 import utilities as util
+import dataset_constants as DSC
+from keras.preprocessing.image import ImageDataGenerator
 
-util.print_dataset_stats( '../set-A_train/')
-util.print_dataset_stats( '../set-B_test/')
+( input_array, outputs) = util.load_samples( '../set-B_test/')
 
-( input_A, output_A) = util.load_samples( '../set-A_train/')
-( input_B, output_B) = util.load_samples( '../set-A_train/')
+#generator = ImageDataGenerator( rotation_range = 2,
+#                                zoom_range = 0.02,
+#                                width_shift_range = 0.02,
+#                                height_shift_range = 0.02,
+#                                fill_mode = 'constant',
+#                                featurewise_center=True,
+#                                featurewise_std_normalization=True)
+#generator.fit( input_array)
+#
+#for ( x_batch, y_batch) in generator.flow( input_array, outputs[0],
+#                                           batch_size = 64) :
+#    break
