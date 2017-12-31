ede271078e646e611911605307068328451620df
Luis I. Reyes-Castro <luis.i.reyes.castro@gmail.com>
2017-12-31 01:41:19 -0500

    Adds methods save_model and load_model, and adds model-checkpoint and early-stopping callbacks to method train.

--- scaled-models/examples/example_models.py
+++ scaled-models/examples/example_models.py
@@ -15,7 +15,7 @@ from keras.layers import Flatten, Dense
 from keras.regularizers import l2
 from keras.optimizers import SGD
 from keras.utils.vis_utils import plot_model
-from keras.callbacks import TensorBoard
+from keras.callbacks import ModelCheckpoint, TensorBoard, EarlyStopping
 from keras.preprocessing.image import ImageDataGenerator
 
 import utilities as utils
@@ -78,7 +78,7 @@ class DetectorCNN :
         layer_2B = Dropout( rate = dropout_rate,
                             name = 'Dropout-2' )( layer_2A )
 
-        layer_3A = Conv2D( filters = 128,
+        layer_3A = Conv2D( filters = 32,
                            kernel_size = (3,3), strides = (2,2),
                            name = 'Convolution-3',
                            activation = 'relu',
@@ -128,27 +128,22 @@ class DetectorCNN :
         output_tensors = [ output_prob, output_ship,
                            output_row, output_col, output_head ]
 
-        loss = { 'Output_Ship-Prob' : 'binary_crossentropy',
-                 'Output_Ship'      : 'categorical_crossentropy',
-                 'Output_Row'       : 'categorical_crossentropy',
-                 'Output_Col'       : 'categorical_crossentropy',
-                 'Output_Head'      : 'categorical_crossentropy' }
-
-        loss_weights = { 'Output_Ship-Prob' : 8.0,
-                         'Output_Ship'      : 1.0,
-                         'Output_Row'       : 2.0,
-                         'Output_Col'       : 2.0,
-                         'Output_Head'      : 1.0 }
-
         self.model = Model( inputs = layer_00, outputs = output_tensors)
 
-        optimizer = SGD( lr = learning_rate,
-                         momentum = 0.9, nesterov = True )
+        self.loss = { 'Output_Ship-Prob' : 'binary_crossentropy',
+                      'Output_Ship'      : 'categorical_crossentropy',
+                      'Output_Row'       : 'categorical_crossentropy',
+                      'Output_Col'       : 'categorical_crossentropy',
+                      'Output_Head'      : 'categorical_crossentropy' }
 
-        self.model.compile( optimizer = optimizer,
-                            loss = loss,
-                            loss_weights = loss_weights,
-                            metrics = ['accuracy'] )
+        self.loss_weights = { 'Output_Ship-Prob' : 8.0,
+                              'Output_Ship'      : 1.0,
+                              'Output_Row'       : 2.0,
+                              'Output_Col'       : 2.0,
+                              'Output_Head'      : 1.0 }
+
+        self.optimizer = SGD( lr = learning_rate,
+                              momentum = 0.9, nesterov = True )
 
         return
 
@@ -159,48 +154,100 @@ class DetectorCNN :
         return plot_model( model = self.model,
                            show_shapes = True, to_file = filename)
 
+    def save_model( self) :
+
+        print( 'Saving DetectorCNN model definition to file:',
+               self.model_definition )
+
+        model_dict = { 'batch_size' : self.batch_size,
+                       'learning_rate' : self.learning_rate,
+                       'regularization_rate' : self.regularization_rate,
+                       'dropout_rate' : self.dropout_rate,
+                       'model_definition' : self.model_definition,
+                       'model_weights' : self.model_weights,
+                       'model_tb_log' : self.tb_log_dir }
+
+        utils.ensure_directory( self.MODEL_DIR)
+        utils.serialize( model_dict, self.model_definition)
+
+        return
+
+    @staticmethod
+    def load_model( model_definition_file) :
+
+        if not utils.exists_file( model_definition_file) :
+            raise ValueError( 'Did not find file:',
+                              str(model_definition_file) )
+
+        model_def = utils.de_serialize( model_definition_file)
+
+        dcnn = DetectorCNN( model_def['batch_size'],
+                            model_def['learning_rate'],
+                            model_def['regularization_rate'],
+                            model_def['dropout_rate'] )
+
+        dcnn.model.load_weights( model_def['model_weights'] )
+
+        return dcnn
+
     def train( self, batch_size = 16,
-                     epochs = 200,
+                     epochs = 400,
+                     patience = None,
                      workers = 4,
-                     split_data = True,
-                     augment_validation_data = True ) :
+                     augment_validation_data = True,
+                     monitor = 'val_Output_Row_acc' ) :
+
+        self.save_model()
+        self.model.compile( optimizer = self.optimizer,
+                            loss = self.loss,
+                            loss_weights = self.loss_weights,
+                            metrics = ['accuracy'] )
+
+        call_mc = ModelCheckpoint( filepath = self.model_weights,
+                                   monitor = monitor,
+                                   save_weights_only = True,
+                                   save_best_only = True )
 
         utils.ensure_directory( self.tb_log_dir)
+        call_tb = TensorBoard( log_dir = self.tb_log_dir,
+                               write_graph = False )
 
-        callbacks = [ TensorBoard( log_dir = self.tb_log_dir,
-                                   write_graph = False ) ]
+        callbacks = [ call_mc, call_tb]
 
-        if split_data :
+        if patience is not None and isinstance( patience, int) :
+            call_es = EarlyStopping( monitor = monitor,
+                                     patience = patience )
+            callbacks.append( call_es)
 
-            ( X_t, Y_t) = utils.load_samples( DSC.SET_A_DIR)
-            ( X_v, Y_v) = utils.load_samples( DSC.SET_B_DIR)
+        ( X_t, Y_t) = utils.load_samples( DSC.SET_A_DIR)
+        ( X_v, Y_v) = utils.load_samples( DSC.SET_B_DIR)
 
-            self.X_mean = X_t.mean()
-            X_t -= self.X_mean
-            X_v -= self.X_mean
+        self.X_mean = X_t.mean()
+        X_t -= self.X_mean
+        X_v -= self.X_mean
 
-            self.X_std = X_t.std()
-            X_t /= self.X_std
-            X_v /= self.X_std
+        self.X_std = X_t.std()
+        X_t /= self.X_std
+        X_v /= self.X_std
 
-            t_data = self.batch_generator( X_t, Y_t, batch_size)
+        t_data = self.batch_generator( X_t, Y_t, batch_size)
 
-            if augment_validation_data :
-                v_data = self.batch_generator( X_v,  Y_v,  batch_size)
-            else :
-                v_data = ( X_v,  Y_v)
+        if augment_validation_data :
+            v_data = self.batch_generator( X_v,  Y_v,  batch_size)
+        else :
+            v_data = ( X_v,  Y_v)
 
-            t_steps = X_t.shape[0] // batch_size
-            v_steps = X_v.shape[0] // batch_size
+        t_steps = X_t.shape[0] // batch_size
+        v_steps = X_v.shape[0] // batch_size
 
-            self.model.fit_generator( generator = t_data,
-                                      steps_per_epoch = t_steps,
-                                      validation_data = v_data,
-                                      validation_steps = v_steps,
-                                      epochs = epochs,
-                                      use_multiprocessing = True,
-                                      workers = workers,
-                                      callbacks = callbacks )
+        self.model.fit_generator( generator = t_data,
+                                  steps_per_epoch = t_steps,
+                                  validation_data = v_data,
+                                  validation_steps = v_steps,
+                                  epochs = epochs,
+                                  use_multiprocessing = True,
+                                  workers = workers,
+                                  callbacks = callbacks )
 
         return
 
